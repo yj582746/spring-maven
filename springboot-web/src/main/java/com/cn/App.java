ceeb4cd74cdb952c64763fe711c2552a4d416b7c
xh <xh@xh-PC>
2017-06-05 22:28:58 +0800

    yj

---
+++ springboot-web/src/main/java/com/cn/App.java
@@ -0,0 +1,18 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
---
+++ springboot-web/src/main/java/com/cn/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.cn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/test")
+	public String test() {
+		return "webapp start..";
+	}
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World!!") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "page/hello";
+	}
+}
